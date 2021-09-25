cad5cb073953bd76d36e46480cd153c667175b4a
marrsd <mpenaranda1@misena.edu.co>
2021-09-25 15:18:57 -0500

    create index.js file, create controllers folder and models folder

---
+++ JavaScript/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const userRoutes = require('./src/controllers/user.controller');
+const proyectRoutes = require('./src/controllers/proyect.controller');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const DB_ATLAS = process.env.ATLAS_DB_CONNECTION;
+
+app.use(cors());
+app.use(express.json());
+
+//Connection to mongodb Atlas with mongoose
+mongoose.connect(DB_ATLAS);
+mongoose.connection.once('open', () => console.log('DB Connected'));
+
+//Route path on proyect
+app.get('/', (req, res) => {
+  res.send('Proyect Managment App Mintic running');
+});
+
+//Routes path on users
+// app.use('/users', userRoutes); ----------> //DESCOMENTAR CUANDO SE CREEN LOS CONTROLLERS Y LOS MODELS
+
+//Routes path on proyects
+// app.use('/proyects', proyectRoutes); ----------> //DESCOMENTAR CUANDO SE CREEN LOS CONTROLLERS Y LOS MODELS
+
+app.listen(PORT, () => {
+  console.log(`app listening at port ${PORT}`);
+});
